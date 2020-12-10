77c63cfc6d52ed77ad9597d6811e66f45685008e
Riccardo Albiero <albr.riccardo@gmail.com>
2020-12-10 18:56:12 +0100

    added keybindings and TODO section

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -3,13 +3,21 @@
 
 from typing import List  # noqa: F401
 
-from libqtile import bar, layout, widget
+import os
+import socket
+import subprocess
+# from subprocess import call, check_output
+
+from libqtile import bar, layout, widget, extension
 from libqtile.config import Click, Drag, Group, Key, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
-import os
-import subprocess
 from libqtile import hook
+
+# TODO:
+# * fix floating blue border
+# * add audio notification with dunst
+# * polish bar
+# * add redshift toggle button
 
 
 @hook.subscribe.startup_once
@@ -36,63 +44,147 @@ def dbus_register():
                       'string:' + id])
 
 
-mod = "mod1"                # MODIFIER = ALT
+class Commands:
+    alsamixer = ' -e alsamixer'
+    volume_up = 'amixer -q -c 0 sset Master 2dB+'
+    volume_down = 'amixer -q -c 0 sset Master 2dB-'
+    volume_toggle = 'amixer -q set Master toggle'
+    # mic_toggle = 'amixer -q set Dmic0 toggle'
+    screenshot_all = 'sscrot'
+    screenshot_window = 'sscrot u'
+    screenshot_selection = 'sscrot s'
+
+
+commands = Commands()
+
+prompt_cmd = "{0}@{1}".format(os.environ["USER"], socket.gethostname())
+
+# key macros
+ALT = 'mod1'
+WIN = 'mod4'
+TAB = 'Tab'
+CTRL = 'control'
+SHIFT = 'shift'
+RETURN = 'Return'
+SPACE = 'space'
+PRINT = 'Print'
+MODKEY = ALT
+
 # terminal = 'alacritty'     # DEFAULT TERMINAL
 terminal = 'kitty'          # DEFAULT TERMINAL
 
 keys = [
-    # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(), desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(), desc="Move focus up in stack pane"),
+
+    # WINDOW MANAGEMENT ############
+
+    Key([MODKEY, CTRL], "r", lazy.restart(), desc="Restart qtile"),
+    Key([MODKEY, CTRL], "q", lazy.shutdown(), desc="Shutdown qtile"),
+
+    # Move focus
+    Key([MODKEY], "h", lazy.layout.left()),
+    Key([MODKEY], "l", lazy.layout.right()),
+    Key([MODKEY], "j", lazy.layout.down()),
+    Key([MODKEY], "k", lazy.layout.up()),
 
     # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
-        desc="Move window down in current stack "),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
-        desc="Move window up in current stack "),
+    Key([MODKEY, SHIFT], "h", lazy.layout.swap_left()),
+    Key([MODKEY, SHIFT], "l", lazy.layout.swap_right()),
+    Key([MODKEY, SHIFT], "j", lazy.layout.shuffle_down()),
+    Key([MODKEY, SHIFT], "k", lazy.layout.shuffle_up()),
+    Key([MODKEY], 'i', lazy.layout.swap_main()),
 
-    # Switch window focus to other pane(s) of stack
-    Key([mod], "space", lazy.spawn('rofi -show drun'),
-        desc="Switch window focus to other pane(s) of stack"),
+    # Toggle floating / fullscreen
+    Key([MODKEY], "t", lazy.window.toggle_floating(),
+        desc="Toggle floating window"),
+    Key([MODKEY, SHIFT], "f", lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen"),
 
-    # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate(),
-        desc="Swap panes of split stack"),
+    # Toggle between different layouts
+    Key([MODKEY], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
 
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    # GAPS and RESIZING ############
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([mod], "r", lazy.spawncmd(),
+    Key([MODKEY, CTRL], "k", lazy.layout.grow()),
+    Key([MODKEY, CTRL], "j", lazy.layout.shrink()),
+    Key([MODKEY, SHIFT], 'n', lazy.layout.reset()),
+    Key([MODKEY], "m", lazy.layout.maximize()),
+    Key([MODKEY], "o", lazy.layout.flip()),
+
+    # LAUNCHERS AND PROGRAMS #######
+
+    Key([MODKEY], "space", lazy.spawn('rofi -show drun'),
+        desc="Rofi application launcher"),
+    Key([MODKEY], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([MODKEY], "w", lazy.spawn('brave'), desc="Launch browser"),
+    Key([MODKEY], "e", lazy.spawn(terminal + " -e mutt"),
+        desc="Launch email-client"),
+    Key([MODKEY], "f", lazy.spawn(terminal + " -e ranger"),
+        desc="Launch filemanager"),
+
+    # Volume control
+    Key([], "F10", lazy.spawn(Commands.volume_toggle),
+        desc='Mute volume'),
+    Key([], "F11", lazy.spawn(Commands.volume_down),
+        desc='Turn down volume'),
+    Key([], "F12", lazy.spawn(Commands.volume_up),
+        desc='Turn up volume'),
+
+    # Screenshot
+    Key([], "Print", lazy.spawn(Commands.screenshot_selection),
+        desc='scrot selection'),
+    Key([SHIFT], "Print", lazy.spawn(Commands.screenshot_all),
+        desc='scrot fullscreen'),
+    Key([CTRL], "Print", lazy.spawn(Commands.screenshot_window),
+        desc='scrot focused window'),
+
+    # Kill and Restart
+    Key([MODKEY], "r", lazy.spawncmd(prompt=prompt_cmd),
         desc="Spawn a command using a prompt widget"),
+    Key([MODKEY], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([MODKEY, SHIFT], "q", lazy.run_extension(extension.CommandSet(
+        commands={
+            'poweroff': 'systemctl poweroff',
+            'restart': 'reboot',
+            'logout': 'qtile-cmd -o cmd -f shutdown',
+            'reload': 'qtile-cmd -o cmd -f restart',
+        },
+        foreground='#863034',
+        selected_background='#863034',
+        font='Iosevka Heavy',
+        fontsize=11,)),
+        desc='dmenu session manager'),
 ]
 
-groups = [Group(i) for i in "123456789"]
+# Mouse bindings
+mouse = [
+    Drag([MODKEY], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([MODKEY], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([MODKEY], "Button2", lazy.window.bring_to_front())
+]
+
+
+# Groups
+groups = [
+    Group('1'),
+    Group('2'),
+    Group('3'),
+    Group('4'),
+    Group('5'),
+    Group('6'),
+    Group('7'),
+    Group('8'),
+    Group('9'),
+]
 
 for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-    ])
+    keys.append(Key([MODKEY], i.name, lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name)))
+    keys.append(Key([MODKEY, SHIFT], i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(i.name)))
 
 layouts = [
     layout.MonadTall(
@@ -110,7 +202,8 @@ layouts = [
     ),
     layout.Floating(
         auto_float_types='dialog',
-        border_focus='#505050',
+        border_focus='#303030',
+        border_normal='#101010',
         name='f',
     ),
 ]
@@ -128,12 +221,12 @@ screens = [
             [
                 widget.Sep(
                     linewidth=0,
-                    padding=8,
+                    padding=10,
                 ),
                 widget.Clock(format='%a %d/%m, %H:%M'),
                 widget.Sep(
                     linewidth=0,
-                    padding=8,
+                    padding=10,
                 ),
                 widget.Prompt(),
                 widget.Spacer(),
@@ -151,6 +244,10 @@ screens = [
                 widget.TextBox(
                     text='',
                     fontsize=13,
+                    mouse_callbacks={
+                        'Button1': lambda qtile: qtile.cmd_spawn(terminal +
+                                                                 commands.alsamixer),
+                    },
                 ),
                 widget.Volume(),
                 widget.Systray(
@@ -166,7 +263,7 @@ screens = [
                 ),
                 widget.Sep(
                     linewidth=0,
-                    padding=6,
+                    padding=10,
                 ),
             ],
             24,
@@ -174,20 +271,11 @@ screens = [
     ),
 ]
 
-# Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
-]
-
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = False
-bring_front_click = False
+bring_front_click = True
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
