f79f520d3759c5ea51b7a37d735b1d05501af169
ricalbr <albr.riccardo@gmail.com>
2021-12-18 12:32:44 +0100

    Add restart shell alias

--- .config/zsh/aliases/bare-git.zsh
+++ .config/zsh/aliases/bare-git.zsh
@@ -2,12 +2,12 @@
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias ca='config add'
-alias ccm='config commit -v'
-alias ccm!='config commit -v --amend'
+alias ccm=config_commit
+alias ccm!=config_commit_amend
 alias ccma='config commit -v -a'
 alias ccma!='config commit -v -a --amend'
 alias ccmam='config commit -v -a -m'
-alias cdiff='config diff'
+alias cdiff=config_diff
 alias cmv='config mv'
 alias cpull='config pull --recurse-submodules -v'
 alias cpush='config push -v'
@@ -16,7 +16,23 @@ alias csu='config submodule update --remote --merge'
 alias cup='config pull --rebase -v'
 function csa() { config submodule add ssh://git@github.com/"$*" }
 
-# lazy updates
+# quick updates
 alias cpac='config commit ~/.config/pkglist-pacman.txt -m "Update pacman package list"'
 alias cyay='config commit ~/.config/pkglist-yay.txt -m "Update yay package list"'
 alias ccv='config commit ~/.config/vim -m "Update Vim module"'
+
+function config_commit(){
+  config status -s | cut -c12- |\
+  fzf --multi --no-sort --reverse --bind 'enter:execute(/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME commit -v {})'
+}
+
+function config_commit_amend(){
+  config status -s | cut -c12- |\
+  fzf --multi --no-sort --reverse --bind 'enter:execute(/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME commit -v --amend {})'
+}
+
+function config_diff(){
+  config status -s | cut -c12- |\
+  fzf --multi --no-sort --reverse --bind 'enter:execute(/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME diff {})'
+}
+
