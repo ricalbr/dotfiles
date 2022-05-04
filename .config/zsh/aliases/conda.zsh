3067c585c44f6e5b3d118b22a095eb159edb74ba
ricalbr <albr.riccardo@gmail.com>
2022-05-04 14:54:30 +0200

    Add uninstall alias

--- .config/zsh/aliases/conda.zsh
+++ .config/zsh/aliases/conda.zsh
@@ -4,6 +4,7 @@ alias cda='conda activate'          # activate environment
 alias cdd='conda deactivate'        # deactivate environment
 alias cdl='conda envs list'         # list all environments
 alias ccenv='conda create -n'       # conda create new environment
+alias cdu='conda uninstall'         # uninstall package
 
 # conda install from conda-forge
 cdi() { conda install -c conda-forge "$@" ;}
