82fdf18c4fbfb01481a7d5031469fdecb1275c05
ricalbr <albr.riccardo@gmail.com>
2022-05-19 22:28:25 +0200

    Update conda aliases

--- .config/zsh/aliases/conda.zsh
+++ .config/zsh/aliases/conda.zsh
@@ -1,17 +1,17 @@
 # CONDA ALIASES
 
-alias cnda='conda activate'          # activate environment
-alias cndd='conda deactivate'        # deactivate environment
-alias cndl='conda envs list'         # list all environments
-alias cndc='conda create -n'       # conda create new environment
-alias cndu='conda uninstall'         # uninstall package
+alias cda='conda activate'          # activate environment
+alias cdd='conda deactivate'        # deactivate environment
+alias cdl='conda envs list'         # list all environments
+alias cdcenv='conda create -n'      # conda create new environment
+alias cdu='conda uninstall'         # uninstall package
 
 # conda install from conda-forge
-cndi() { conda install -c conda-forge "$@" ;}
+cdi() { conda install -c conda-forge "$@" ;}
 
 # conda update
-cndup() { conda update --name "$@" --all;}
+cdup() { conda update --name "$@" --all;}
 
 # remove environment
-cndrenv() { conda remove --name "$@" --all;}
+cdrenv() { conda remove --name "$@" --all;}
 
