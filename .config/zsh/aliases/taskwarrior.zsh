6b4db9093bacdedd773d7aeaa17bbf5b47bbad76
ricalbr <albr.riccardo@gmail.com>
2025-09-17 07:19:58 +0200

    (style) remove trailing white spaces

--- .config/zsh/aliases/taskwarrior.zsh
+++ .config/zsh/aliases/taskwarrior.zsh
@@ -1,13 +1,14 @@
 # TASKWARRIOR ALIASES
+alias task='task 2>/dev/null'
 
-alias tt='clear;task next'         
-alias tw='clear;task +work next'         
-alias ta='task add'       
+alias tt='clear;task next'
+alias tw='clear;task +work next'
+alias ta='task add'
 alias tm='task mod'
 alias tn='task mod +next'
 alias td='task done -next'
 
-alias tapl='task add pri:L '      
-alias tapl='task add pri:H '      
-alias tap='task add +personal'
-alias taw='task add +work'
+alias tapl='task add pri:L '
+alias taph='task add pri:H '
+alias tatp='task add +personal'
+alias tatw='task add +work'
