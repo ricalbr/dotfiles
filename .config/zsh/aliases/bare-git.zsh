8acabf775fa9dbe160f6b5c0ce474f9e3a38fbf1
ricalbr <albr.riccardo@gmail.com>
2025-10-01 21:58:06 +0200

    (fix) adapt all aliases for dot

--- .config/zsh/aliases/bare-git.zsh
+++ .config/zsh/aliases/bare-git.zsh
@@ -1,27 +1,27 @@
 # GIT-BARE ALIASES (for .dotfiles)
 
 alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias ca=dot_add
-alias ccm=dot_commit
-alias ccm!=dot_commit_amend
-alias ccma='dot commit -v -a'
-alias ccma!='dot commit -v -a --amend'
-alias ccmam='dot commit -v -a -m'
-alias clogs="dot log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat --max-count=15"
-alias cdiff=dot_diff
-alias cmv='dot mv'
-alias cpull='dot pull --recurse-submodules -v'
-alias cpush='dot push -v'
-alias cs='dot status -s'
-alias csu='dot submodule update --remote --merge'
-alias cup='dot pull --rebase -v'
-function csa() { dot submodule add ssh://git@github.com/"$*" }
+alias da=dot_add
+alias dcm=dot_commit
+alias dcm!=dot_commit_amend
+alias dcma='dot commit -v -a'
+alias dcma!='dot commit -v -a --amend'
+alias dcmam='dot commit -v -a -m'
+alias dlogs="dot log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat --max-count=15"
+alias ddiff=dot_diff
+alias dmv='dot mv'
+alias dpull='dot pull --recurse-submodules -v'
+alias dpush='dot push -v'
+alias ds='dot status -s'
+alias dsu='dot submodule update --remote --merge'
+alias dup='dot pull --rebase -v'
+function dsa() { dot submodule add ssh://git@github.com/"$*" }
 
 # quick updates
-alias cnvim='dot commit ~/.config/nvim -m "Update Neovim module"'
-alias cgnome='dot commit ~/.config/gnome_backup.txt -m "Update GNOME setting"'
-alias cpac='dot commit ~/.config/pkglist-pacman.txt -m "Update pacman package list"'
-alias cyay='dot commit ~/.config/pkglist-yay.txt -m "Update yay package list"'
+alias dnvim='dot commit ~/.config/nvim -m "Update Neovim module"'
+alias dgnome='dot commit ~/.config/gnome_backup.txt -m "Update GNOME setting"'
+alias dpac='dot commit ~/.config/pkglist-pacman.txt -m "Update pacman package list"'
+alias dyay='dot commit ~/.config/pkglist-yay.txt -m "Update yay package list"'
 
 # functions
 function dot_add(){
